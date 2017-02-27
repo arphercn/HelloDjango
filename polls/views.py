404d9f734b598ab2cfdaca995aeb5dfb98978a8b
arpher <arpher@qq.com>
2017-02-27 21:47:10 +0800

    delete annotation by arpher

--- polls/views.py
+++ polls/views.py
@@ -1,37 +1,12 @@
-from django.http import HttpResponseRedirect, HttpResponse
-from django.template import RequestContext, loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
 from django.core.urlresolvers import reverse
 from .models import Choice, Question
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     output = ', '.join([p.question_text for p in latest_question_list])
-#     return HttpResponse(output)
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = RequestContext(request, {
-#         'latest_question_list': latest_question_list,
-#     })
-#     return HttpResponse(template.render(context))
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-#     return HttpResponse("You're looking at question %s." % question_id)
-
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, 'polls/detail.html', {'question': question})
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
